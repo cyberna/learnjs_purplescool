655b31229420f3d718371ef7ab8c2c40c8a583d5
na <cyberna@yandex.ru>
2026-05-14 14:31:27 +0400

    Тернарный оператор пример

---
+++ js/ternarn_oper.js
@@ -0,0 +1,18 @@
+// const bmwX3Price = 10000;
+// const fordFocusPrice = 10000;
+// const budget = 20000;
+
+// let message;
+// if (budget > bmwX3Price) {
+//     message = 'BMW';
+// } else if (budget > fordFocusPrice) {
+//     message = 'Ford';
+// } else {
+//     message = 'Велосипед';
+// }
+
+
+// Тернарный оператор
+// let message = budget > bmwX3Price ? "BMW" : "Велосипед"
+
+// console.log(`Я хочу купить ${message}`);
