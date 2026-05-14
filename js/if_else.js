d91555f7fc80c0cb68c5ec8d9c8489b53b5d0c82
na <cyberna@yandex.ru>
2026-05-14 11:03:14 +0400

    Добавил файлы прохождения курса if_else, switch_case, ternarn_oper

---
+++ js/if_else.js
@@ -0,0 +1,11 @@
+const role = "admin";
+
+if(role=="admin") {
+    console.log(`Привет ${role}, вы админ`);
+} else if (role=="manager") {
+    console.log(`Привет ${role}, вы менеджер`);
+} else if (role == "CEO") {
+    console.log(`Привет ${role}, вы СЕО`);
+} else {
+    console.log(`${role}, мы о вас не знаем`);
+}
---
+++ js/siwtch_case.js
@@ -0,0 +1,27 @@
+const role = "admin";
+
+switch(role) {
+    case "admin":
+        console.log(`Добро пожаловать ${role}`);
+        break;
+    case "manager":
+        console.log(`Добро пожаловать ${role}`);
+        break;
+    case "CEO":
+        console.log(`Добро пожаловать ${role}`);
+        break;
+    default:
+        console.log(`Мы о вас не знаем`);
+}
+
+
+// Сокращенный вариант
+switch(role) {
+    case "admin":
+    case "manager":
+    case "CEO":
+        console.log(`Добро пожаловать ${role}`);
+        break;
+    default:
+        console.log(`Мы о вас не знаем ${role}`);
+}
