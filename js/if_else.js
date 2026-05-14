a1105802f11b1573593c31cf87e1d667e11fff71
na <cyberna@yandex.ru>
2026-05-14 11:25:54 +0400

    Исправил 1

--- js/if_else.js
+++ js/if_else.js
@@ -1,11 +1,11 @@
-const role = "admin";
+// const role = "Alex";
 
-if(role=="admin") {
-    console.log(`Привет ${role}, вы админ`);
-} else if (role=="manager") {
-    console.log(`Привет ${role}, вы менеджер`);
-} else if (role == "CEO") {
-    console.log(`Привет ${role}, вы СЕО`);
-} else {
-    console.log(`${role}, мы о вас не знаем`);
-}
+// if(role=="admin") {
+//     console.log(`Привет ${role}, вы админ`);
+// } else if (role=="manager") {
+//     console.log(`Привет ${role}, вы менеджер`);
+// } else if (role == "CEO") {
+//     console.log(`Привет ${role}, вы СЕО`);
+// } else {
+//     console.log(`${role}, мы о вас не знаем`);
+// }
