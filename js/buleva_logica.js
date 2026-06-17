bbaf58d74e681ef01a2140351c59e61a6b9e8aaf
na <cyberna@yandex.ru>
2026-06-17 14:21:41 +0400

    Buleva 2

--- js/buleva_logica.js
+++ js/buleva_logica.js
@@ -10,15 +10,23 @@
 // console.log('Вася' || false);
 // console.log(false || false);
 
-console.log('Вася' && 'Олег');
-console.log(false && 'Олег');
-console.log('Вася' && false);
-console.log(false && false);
+// console.log('Вася' && 'Олег');
+// console.log(false && 'Олег');
+// console.log('Вася' && false);
+// console.log(false && false);
 
-let a = 'Марина';
-const username = a || 'Петя';
-console.log(username);
+// let a = 'Марина';
+// const username = a || 'Петя';
+// console.log(username);
 
-const isAdmin = true;
-const filename = isAdmin && 'file.mp4';
-console.log(filename);
+// const isAdmin = true;
+// const filename = isAdmin && 'file.mp4';
+// console.log(filename);
+
+let isAdmin = 0;
+let realB = isAdmin || false;
+console.log(realB);
+
+
+let realB2 = isAdmin ?? false;
+console.log(realB2);
