ca3afa9784218e0a187517eccf0678d1e46ca409
na <cyberna@yandex.ru>
2026-06-30 13:58:27 +0400

    Стрелочные функции

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+// Функции 1
+// const toPowerArrow = (num, power) => {
+//     const res = num ** power;
+//     return res;
+// }
+
+// console.log(toPowerArrow(2, 3));
+
+// const toPowerArrow = (num, power) => num ** power;
+// console.log(toPowerArrow(2, 3));
