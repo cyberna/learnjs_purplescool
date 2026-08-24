b33d3beec38829699485360181b4abf066ef3118
na <cyberna@yandex.ru>
2026-08-24 08:39:22 +0400

    Основные методы массивов

--- js/massive.js
+++ js/massive.js
@@ -1,25 +1,32 @@
-const role1 = "admin";
-const role2 = "user";
-const role3 = "superuser";
+// const role1 = "admin";
+// const role2 = "user";
+// const role3 = "superuser";
 
-const roles = ['admin', 'user', 'superuser'];
-const userInfo = ['Аня', 25];
-console.log(roles);
-console.log(roles[0]);
-console.log(roles.length);
-console.log(roles[roles.length - 1]);
+// const roles = ['admin', 'user', 'superuser'];
+// const userInfo = ['Аня', 25];
+// console.log(roles);
+// console.log(roles[0]);
+// console.log(roles.length);
+// console.log(roles[roles.length - 1]);
 
-console.log(roles.at(0)); // Современный вариант записи в node.js
-console.log(roles.at(-1)); // Компактнее
+// console.log(roles.at(0)); // Современный вариант записи в node.js
+// console.log(roles.at(-1)); // Компактнее
 
-const usersAGe = [2040 - 2022, 10 > 0 ? 5 : 0];
-console.log(usersAGe);
+// const usersAGe = [2040 - 2022, 10 > 0 ? 5 : 0];
+// console.log(usersAGe);
 
-const userNames = new Array('Вася', 'Петя', 'Катя');
-console.log(userNames);
+// const userNames = new Array('Вася', 'Петя', 'Катя');
+// console.log(userNames);
 
-function square(el) {
-    return el * el;
-}
+// function square(el) {
+//     return el * el;
+// }
 
-console.log(square([1, 2, 3]));
+// console.log(square([1, 2, 3]));
+
+// Метод push добавляет элемент в конец массива и возвращает новую длину массива.
+// Метод unshift добавляет элемент в начало массива, смещая индексы всех остальных элементов.
+// Метод pop удаляет последний элемент массива и возвращает этот удалённый элемент.
+// Метод shift удаляет первый элемент массива, смещает оставшиеся элементы к началу и возвращает удалённый элемент.
+// Все четыре метода изменяют исходный массив напрямую (мутируют его), а не создают новый.
+
